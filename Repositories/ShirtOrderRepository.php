40be0a226c312f73d62ff3fb051760779381b0f7
Alireza Bashiri <al3rez@gmail.com>
2022-07-30 04:47:34 +0700

    fix typo

--- Repositories/ShirtOrderRepository.php
+++ Repositories/ShirtOrderRepository.php
@@ -5,22 +5,22 @@ use Befeni\Entities\ShirtOrder;
 class ShirtOrderRepository implements IRepository {
     protected $db;
 
-    public function __construct(Database $db) {
+    public function __construct(IDatabase $db) {
         $this->db = $db;
     }
 
     public function find(ShirtOrder $shirtOrder)
     {
-        $this.db->find($shirtOrder);
+        $this->db->find($shirtOrder);
     }
 
     public function save(ShirtOrder $shirtOrder)
     {
-        $this.db->save($shirtOrder);
+        $this->db->save($shirtOrder);
     }
 
     public function remove(ShirtOrder $shirtOrder)
     {
-        $this.db->remove($shirtOrder);
+        $this->db->remove($shirtOrder);
     }
 }
